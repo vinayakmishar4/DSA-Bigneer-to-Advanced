4e0ebfbafbd13fc41462a197a5dfbb86ed86c771
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-10 15:16:11 +0530

    Create For_Loop.java

---
+++ For_Loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+// for loop
+class Loop2
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scannner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+       System.out.println(i);
+    }
+  }
+}
