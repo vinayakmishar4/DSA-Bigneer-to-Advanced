96ccb35d22b3b5365b1559f90eb4269d577252e9
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-18 13:36:21 +0530

    Create Pattern12.java

---
+++ Pattern12.java
@@ -0,0 +1,27 @@
+ // WAP to  Number Crown Pattern
+import java.util.Scanner;
+class Pattern12
+{
+  public static void main(String []args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number :");
+    int a=sc.nextInt();
+    System.out.println("Number Crown Pattern");
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=i;j++)
+      {
+         System.out.println(j);
+      }
+      for(int k=1;k<=2*n-2*i;K++)
+      {
+         System.out.println("-")  
+      }
+      for(int l=i;l>=1:l--)
+      {
+        System.out.println(l);  
+      }
+    }
+  }
+}
