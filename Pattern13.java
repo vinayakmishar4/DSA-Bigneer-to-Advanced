1b8c4caf1f1b789de3d00a6281e088e4d20132b5
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-18 14:55:19 +0530

    Create Pattern13.java

---
+++ Pattern13.java
@@ -0,0 +1,11 @@
+// WAP to Increasing Number Triangle Pattern
+class Pattern13
+{
+  public static void main(String []args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number")
+    int a=sc.nextInt();
+    System.out.println("Increasing Number Triangle Pattern");
+  }
+}
