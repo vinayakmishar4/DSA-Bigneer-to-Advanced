efd6bb9eaa47595239bb6d4fa2a6739f766a0961
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-19 11:59:13 +0530

    Create Pattern14.java

---
+++ Pattern14.java
@@ -0,0 +1,19 @@
+// WAP to Increasing Letter Triangle Pattern
+class Pattern14
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Number");
+    int a=sc.nextInt();
+    System.out.println("Increasing Letter Triangle Pattern");
+    for(int i=0;i<a;i++)
+    {
+      for(char c='A';c<'A'+i;c++)
+      {
+          System.out.print(c);
+      }
+      System.out.println();
+    }
+  }
+}
