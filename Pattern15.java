f11965543f548bbb10b5d530858087afd234705b
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-19 12:32:00 +0530

    Create Pattern15.java

---
+++ Pattern15.java
@@ -0,0 +1,21 @@
+// WAP to Reverse Letter Triangle Pattern
+import java.util.Scanner;
+
+class Pattern15
+{
+  public static void main(Stringp[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number");
+    int a=sc.nextInt();
+    System.out.println("Reverse Letter Triangle Pattern");
+    for(int i=0;i<a;i++)
+    {
+      for(char ch = 'A'; ch<='A'+(N-i-1);ch++)
+      {
+          System.out.println(ch);
+      }
+      System.out.println();
+    }
+  }
+}
