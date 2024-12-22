110486546fce568c964c0b9cd0980246b4329cf3
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-22 16:38:06 +0530

    Update Pattern21.java

--- Pattern21.java
+++ Pattern21.java
@@ -9,5 +9,20 @@ public class Pattern21
     System.out.println("Enter number");
     int n=scanner.nextInt();
     System.out.println("Hollow Rectangle Pattern");
+    for (int i=0; i<n; i++)
+    {
+      for (int j=0; j<n; j++)
+      {
+        if (i==0 || i==n-1 || j==0 || j==n-1)
+        {
+          System.out.print("*");
+        }
+        else
+        {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
   }  
 }
