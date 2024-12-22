1ddb9703b7ef1b0942a8a91d9ed6f01e0b0d3556
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-22 06:23:22 +0530

    Create Pattern21.java

---
+++ Pattern21.java
@@ -0,0 +1,13 @@
+// WAP to Hollow Rectangle Pattern
+import java.util.Scanner;
+
+public class Pattern21 
+{
+  public static void main(String[] args)
+  {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number");
+    int n=scanner.nextInt();
+    System.out.println("Hollow Rectangle Pattern");
+  }  
+}
