71903ba3b957f5defe3b3b2d038a5e235cad761c
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-23 18:19:32 +0530

    Create Count_Digits.java

---
+++ Count_Digits.java
@@ -0,0 +1,10 @@
+// WAP to count Digits in number
+import java.util.Scanner;
+class CDigits
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter ");
+  }
+}
