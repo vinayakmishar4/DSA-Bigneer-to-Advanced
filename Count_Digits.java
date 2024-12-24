f6e63200548b3a12b65b0353e17ca67349a2f6c0
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-24 10:49:55 +0530

    Update Count_Digits.java

--- Count_Digits.java
+++ Count_Digits.java
@@ -5,6 +5,17 @@ class CDigits
   public static void main(String[] args)
   {
     Scanner sc=new Scanner(System.in);
-    System.out.println("Enter ");
+    System.out.println("Enter Number");
+    int number=sc.nextInt();
+    int sum=0;
+    int count=0;
+    while(num>0)
+    {
+      int r=n%10;
+      sum=sum+r
+      count=count+1;
+    }
+    System.out.println("Sum of Digits in a number = " + sum);
+    System.out.println("Number  of Digits in a number = " + count); 
   }
 }
