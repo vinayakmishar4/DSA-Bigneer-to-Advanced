935a444e8bec652ca80783f1355ac4cd73e1cca1
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-26 15:40:22 +0530

    Create Prime.java

---
+++ Maths/Prime.java
@@ -0,0 +1,62 @@
+// WAP to find prime number
+import java.util.Scanner;
+class Prime
+{
+  static void Brute(int num)
+  {
+    int count=0;
+    for(int i=0;i<num;i++)
+    {
+      if(num%i==0)
+      {
+        count=count+1;
+      }
+    }
+    if(count==2)
+    {
+      System.out.println(num + " Number is an Prime number ");
+    }
+    else
+    {
+      System.out.println(num + "Number is an not Prime number");
+    }
+  }
+  
+  static void Optimal(int num)
+  {
+    int count=0;
+    for(int i=0;i<Math.sqrt(num);i++)
+    {
+      if(num%i==0)
+      {
+        count=count+1;
+      }
+    }
+    if(count==2)
+    {
+      System.out.println(num + " Number is an Prime number ");
+    }
+    else
+    {
+      System.out.println(num + "Number is an not Prime number");
+    }
+  }
+  
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number");
+    int num=sc.nextInt();
+    System.out.println("1.Brute Approach")
+    System.out.println("2.Optimal Approach")
+    switch(n)
+    {
+      case 1:System.out.prinhtln("Brute approach");
+              break;
+      case 2:System.out.println("Optimal approach");
+              break;
+      default:System.out.println("Worng Approach");
+              break;
+    }
+  }
+}
