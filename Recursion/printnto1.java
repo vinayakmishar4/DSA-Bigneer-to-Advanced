cb88aee22fe973b46225581c4da3f92fd4ef3e59
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2024-12-30 00:25:33 +0530

    Create printnto1.java

---
+++ Recursion/printnto1.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+import java.util.Scanner;
+
+class printnto1
+{
+   static void printNto1(int number,int count)
+   {
+     if(number<1)
+     {
+        return;
+     }
+     System.out.println(number);
+     printNto1(number-1, count);
+   }
+   public static void main(String[] args) 
+   {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter number");
+      int number = scanner.nextInt();
+      int count=number;
+      printNto1(number, count);
+    }
+}
