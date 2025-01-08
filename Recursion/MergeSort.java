5dbe94f12215df0ee782b759bf87fc4cf6492029
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-08 23:23:15 +0530

    Update MergeSort.java

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -1,13 +1,56 @@
 package Recursion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MergeSort 
+class solutoion
 {
-   static void Msort(int arr[],int start,int end)
-   {
-
-   } 
+    static void Merge(int arr[],int start,int mid,int end)
+    {
+       ArrayList<Integer> temp = new ArrayList<Integer>();
+       int left=start;
+       int right=mid+1;
+       while(left<=mid && right<=end)
+       {
+         if(arr[left]<=arr[right])
+         {
+             temp.add(arr[left]);
+             left++;
+         }
+         else
+         {
+             temp.add(arr[right]);
+             right++;
+         }
+         while (left<=mid) 
+         {
+             temp.add(arr[left]);
+             left++;
+         }
+         while (right<=end) 
+         {
+             temp.add(arr[right]);
+             right++;
+         }
+         for (int i = start; i < end; i++)
+         {
+             arr[i] = temp.get(i - start);
+         }
+       } 
+    }
+    static void Msort(int arr[],int start,int end)
+    {
+        if(start==end)
+        {
+           int mid=(start+end)/2;
+           Msort(arr,start,mid);
+           Msort(arr,mid+1,end);
+           Merge(arr,start,mid,end);
+        }
+    }
+}
+public class MergeSort 
+{ 
    public static void main(String[] args)
    {
       Scanner scanner = new Scanner(System.in);
@@ -24,5 +67,12 @@ public class MergeSort
       {
          System.out.print(arr[i] + " ");
       }
-   } 
+      System.out.println();
+      extracted(arr);
+      System.out.println("Array after sorting");
+   }
+
+private static void extracted(int[] arr) {
+    solutoion.Msort(arr, 0, arr.length - 1);
+} 
 }
