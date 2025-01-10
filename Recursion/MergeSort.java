785c9904b1c1c526a6ea1617a8cd89ffa7d5b963
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-10 22:24:24 +0530

    Update MergeSort.java

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -3,54 +3,53 @@ package Recursion;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class solutoion
-{
-    static void Merge(int arr[],int start,int mid,int end)
-    {
-       ArrayList<Integer> temp = new ArrayList<Integer>();
-       int left=start;
-       int right=mid+1;
-       while(left<=mid && right<=end)
-       {
-         if(arr[left]<=arr[right])
-         {
-             temp.add(arr[left]);
-             left++;
-         }
-         else
-         {
-             temp.add(arr[right]);
-             right++;
-         }
-         while (left<=mid) 
-         {
-             temp.add(arr[left]);
-             left++;
-         }
-         while (right<=end) 
-         {
-             temp.add(arr[right]);
-             right++;
-         }
-         for (int i = start; i < end; i++)
-         {
-             arr[i] = temp.get(i - start);
-         }
-       } 
-    }
-    static void Msort(int arr[],int start,int end)
-    {
-        if(start==end)
-        {
-           int mid=(start+end)/2;
-           Msort(arr,start,mid);
-           Msort(arr,mid+1,end);
-           Merge(arr,start,mid,end);
-        }
-    }
-}
+
 public class MergeSort 
-{ 
+{
+
+   static void Merge(int arr[],int start,int mid,int end)
+   {
+      ArrayList<Integer> temp = new ArrayList<Integer>();
+      int left=start;
+      int right=mid+1;
+      while(left<=mid && right<=end)
+      {
+        if(arr[left]<=arr[right])
+        {
+            temp.add(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.add(arr[right]);
+            right++;
+        }
+        while (left<=mid) 
+        {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right<=end) 
+        {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = start; i < end; i++)
+        {
+            arr[i] = temp.get(i - start);
+        }
+      } 
+   }
+   static void Msort(int arr[],int start,int end)
+   {
+       if(start==end)
+       {
+          int mid=(start+end)/2;
+          Msort(arr,start,mid);
+          Msort(arr,mid+1,end);
+          Merge(arr,start,mid,end);
+       }
+   } 
    public static void main(String[] args)
    {
       Scanner scanner = new Scanner(System.in);
@@ -68,11 +67,11 @@ public class MergeSort
          System.out.print(arr[i] + " ");
       }
       System.out.println();
-      extracted(arr);
+      Msort(arr, 0, arr.length - 1);
       System.out.println("Array after sorting");
-   }
-
-private static void extracted(int[] arr) {
-    solutoion.Msort(arr, 0, arr.length - 1);
-} 
+      for (int i = 0; i < arr.length; i++)
+      {
+         System.out.print(arr[i] + " ");
+      }
+   } 
 }
