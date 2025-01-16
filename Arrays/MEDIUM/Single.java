d1ee8918d7280b4c29c35cd95626b5ec302357b9
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-16 23:32:13 +0530

    Create Single.java

---
+++ Arrays/MEDIUM/Single.java
@@ -0,0 +1,35 @@
+package Arrays.MEDIUM;
+
+import java.util.Scanner;
+
+public class Single
+{
+    public int singleNumber(int[] nums) 
+    {
+       if(nums.length==1)
+       {
+         return 1;
+       }
+       int xor=0;
+       for(int i=0;i<nums.length;i++)
+       {
+          xor=xor^nums[i];
+       }
+       return xor;
+    }
+    public static void main(String[] args)
+    {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the size of an array");
+       int size = scanner.nextInt();
+       int arr[] = new int[size];
+       System.out.println("Enter the elements of the array");
+       for (int i = 0; i < size; i++)
+       {
+         arr[i] = scanner.nextInt();
+       }
+       Single single = new Single();
+       int result = single.singleNumber(arr);
+       System.out.println("The single number is: " + result);
+    }    
+}
