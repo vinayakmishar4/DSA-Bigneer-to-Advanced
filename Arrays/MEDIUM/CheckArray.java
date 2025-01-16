91b0f1f9acb2dbbbbdc52acd307e3c0b4e8c9ef4
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-16 18:03:37 +0530

    Update CheckArray.java

--- Arrays/MEDIUM/CheckArray.java
+++ Arrays/MEDIUM/CheckArray.java
@@ -1,1 +1,19 @@
-
+// WAP to check wethier array is sorted or not
+class CheckArray
+{
+  static boolean Check(int arr[])
+  {
+     for(int i=0;i<arr.length;i++)
+     {
+        if(arr[i]<arr[i-1])
+        {
+          return false;
+        }
+     }
+     return true;
+  }
+  public static void main(String []args)
+  {
+    
+  }
+}
