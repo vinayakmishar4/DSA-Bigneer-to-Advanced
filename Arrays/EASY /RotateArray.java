8fdf0598d21d34af97f002740c632e745bf9cd74
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-20 15:31:07 +0530

    Update RotateArray.java

--- Arrays/EASY /RotateArray.java
+++ Arrays/EASY /RotateArray.java
@@ -3,6 +3,7 @@ class RotateArrray
   public void rotate(int[] nums, int k)
    {
       int n=nums.length;
+      int[] temp=new int[n];
       k=k%n;
       System.arraycopy(nums,n-k,temp,0,k);
       for(inti=n-k-1;i>=0;i--)
