0f62162350764cfad47cae818c7aa89fe8b34bde
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-20 14:15:01 +0530

    Update RotateArray.java

---
+++ Arrays/EASY /RotateArray.java
@@ -0,0 +1,18 @@
+class RotateArrray
+{
+  public void rotate(int[] nums, int k)
+   {
+      int n=nums.length;
+      k=k%n;
+      System.arraycopy(nums,n-k,temp,0,k);
+      for(inti=n-k-1;i>=0;i--)
+      {
+        nums[k+i]=nums[i];    
+      }
+      System.arraycopy(temp,0,nums,0,k);
+    }
+  public static void main(String []args)
+  {
+    
+  }
+}
