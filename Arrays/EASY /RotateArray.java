865a5f4b8588268498ad0963db1ce1fbdf1ebe46
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-21 21:55:13 +0530

    Update RotateArray.java

--- Arrays/EASY /RotateArray.java
+++ Arrays/EASY /RotateArray.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class RotateArrray
 {
   public void rotate(int[] nums, int k)
@@ -6,7 +8,7 @@ class RotateArrray
       int[] temp=new int[n];
       k=k%n;
       System.arraycopy(nums,n-k,temp,0,k);
-      for(inti=n-k-1;i>=0;i--)
+      for(int i=n-k-1;i>=0;i--)
       {
         nums[k+i]=nums[i];    
       }
@@ -14,6 +16,23 @@ class RotateArrray
     }
   public static void main(String []args)
   {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the size of the array");
+    int n = scanner.nextInt();
+    System.out.println("Enter the elements of the array");
+    int[] nums = new int[n];
+    for(int i=0; i<n; i++)
+    {
+      nums[i] = scanner.nextInt();
+    }
+    System.out.println("Enter the number of positions to rotate");
+    int k = scanner.nextInt();
+    RotateArrray ra = new RotateArrray();
+    ra.rotate(nums, k);
+    System.out.println("Array after rotation: ");
+    for(int i=0; i<n; i++)
+    {
+      System.out.print(nums[i]+" ");
+    }
   }
 }
