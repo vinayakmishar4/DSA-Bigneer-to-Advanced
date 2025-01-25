9aa32489bc563267f342b29979c2ca17b5e539a9
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-25 13:26:49 +0530

    Update MajortyElement.java

--- Arrays/MEDIUM/MajortyElement.java
+++ Arrays/MEDIUM/MajortyElement.java
@@ -1,3 +1,7 @@
+package Arrays.MEDIUM;
+import java.util.Arrays;
+import java.util.Scanner;
+
 class MajortyElement
 {
       public int majorityElement(int[] nums)
@@ -6,5 +10,20 @@ class MajortyElement
        int n=nums.length;
        return nums[n/2];         
     }
+     public static void main(String[] args)
+    {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the size of the array");
+      int n = scanner.nextInt();
+      System.out.println("Enter the elements of the array");
+      int[] nums = new int[n];
+      for(int i=0; i<n; i++)
+      {
+          nums[i] = scanner.nextInt();
+      }
+      MajortyElement me = new MajortyElement();
+      int majority = me.majorityElement(nums);
+      System.out.println("Majority element: " + majority);
+    }   
 }
  
