79a77f551a26671ffd8f08aa178ada11927d61d6
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-01-30 21:13:39 +0530

    Update Countandsay.java

--- STRING/HARD/Countandsay.java
+++ STRING/HARD/Countandsay.java
@@ -1,4 +1,8 @@
 // Write a program  count-and-say sequence is a sequence of digit strings
+package STRING.HARD;
+
+import java.util.Scanner;
+
 class Countandsay 
 {
    public String countAndSay(int n)
@@ -25,5 +29,13 @@ class Countandsay
         result.append(count).append(prev.charAt(prev.length() - 1));
 
         return result.toString();
+    }
+    public static void main(String[] args)
+    {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter a number:");
+      int n = scanner.nextInt();
+      Countandsay obj = new Countandsay();
+      System.out.println("Count-and-say sequence for " + n + "th term: " + obj.countAndSay(n));
     }
 }
