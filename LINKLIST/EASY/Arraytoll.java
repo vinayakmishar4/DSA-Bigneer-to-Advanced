f7ae0634f9687b572b056f11bc491e3d4aa46c13
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-02-05 17:06:42 +0530

    Create Arraytoll.java

---
+++ LINKLIST/EASY/Arraytoll.java
@@ -0,0 +1,40 @@
+// WAP to create the program for array to linked list
+class Arraytoll
+{
+   public static node Frsit1(int arr[])
+   {
+      node head = new node(arr[0]);
+      node mover = head;
+      for(int i=1; i<arr.length; i++)
+      {
+         node temp= new node(arr[i]);
+         mover.next =temp;
+         mover = temp;
+      }
+      return head;
+   }  
+   public static void main(String[] args)
+   {
+     int arr[] = {1, 2, 3, 4, 5};
+     node element =Frsit1(arr);
+     System.out.println("First element of the linked list: " + element.data);
+   }    
+}
+class node
+{
+    int data;// data of the node
+    node next;// next node in the list
+    
+    // Constructor to initialize the node
+    node(int data1,node next1) 
+    {
+        this.data = data1;
+        this.next=next1;
+    }
+    // constructor for next node
+    node(int data1)
+    {
+        this.data = data1;
+        this.next = null;
+    }
+}
