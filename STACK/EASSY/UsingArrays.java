0786309866f66b007c5db9f0fe8028a3030a33da
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2025-02-16 18:23:26 +0530

    Update UsingArrays.java

--- STACK/EASSY/UsingArrays.java
+++ STACK/EASSY/UsingArrays.java
@@ -7,27 +7,37 @@ class UsingArrays
    public static void main(String[] args)
    {
       Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter the size of stack");
+      System.out.println("Enter size of array");
       int size = scanner.nextInt();
+      int arr[] = new int[size];
+      System.out.println("Enter the elements of array");
+      for (int i = 0; i < size; i++)
+      {
+         arr[i] = scanner.nextInt();
+      }
       stimp obj=new stimp();
       System.out.println("Enter the elements of stack");
       for (int i = 0; i < size; i++)
       {
-         obj.push(scanner.nextInt());
+         obj.push(arr[i], arr);
       }
-      System.out.println("Top element of stack is "+obj.top());
-      System.out.println("Popped element is "+obj.pop());
-      System.out.println("Top element of stack is "+obj.top());
+      System.out.println("Top element of stack is "+obj.top(arr));
+      System.out.println("Popped element is "+obj.pop(arr));
+      System.out.println("Top element of stack is "+obj.top(arr));
       System.out.println("Stack sie "+obj.size());
    }
+}
+class UsinaStack
+{
+    // using stack colllection
+
 }
 class stimp
 {
     int top=-1;
     // push elements in the stack   
-    int arr[]=new int[10];
     // function to push an item to stack
-    void push(int x)
+    void push(int x,int arr[])
     {
         if(top>=10)
         {
@@ -38,7 +48,7 @@ class stimp
         arr[top]=x;
     }
     // Function to pop an item from stack
-    int pop()
+    int pop(int arr[])
     {
         if(top==-1)
         {
@@ -50,7 +60,7 @@ class stimp
         return item;
     }
     // top element
-    int top()
+    int top(int[] arr) 
     {
         if(top<0)
         {
